03af245d7c387dd1c2cd0d5351b16da34afeb8d3
Yusuf Fahrizal <yusuffahrizal18@gmail.com>
2022-11-20 00:43:20 +0700

    Button with documentation

--- template/src/components/atoms/Button/index.js
+++ template/src/components/atoms/Button/index.js
@@ -3,35 +3,76 @@ import { Image, StyleSheet, Text, TouchableOpacity } from 'react-native'
 import { COLOR, FONT } from '../../../theme'
 
 const Button = ({ 
-  title = '' /* Required|String */, 
+  title = '' /* Required: if !(icon button)|String */, 
   type = 'primary' /* Required|in ['primary', 'secondary'] */,
   isRounded = false /* Nullable|Boolean */,
-  isIconBtn = false /* Nullable|Boolean */,
+  leftIcon = null /* Nullable|Type File ['png', 'jpg', 'jpeg'] */,
+  rightIcon = null /* Nullable|Type File ['png', 'jpg', 'jpeg'] */,
+  icon = null /* Nullable|Type File ['png', 'jpg', 'jpeg'] */,
+  size = 20 /* Required: if (icon button) */,
   onPress = () => {} /* Required|Function */
  }) => {
 
-  return (
-    <TouchableOpacity style={styles.btnWrapper(type, isRounded)} onPress={test}>
-      <Text style={styles.btnText(type)}>{title}</Text>
-    </TouchableOpacity>
-  )
+  if (icon === null) {
+    return (
+      <TouchableOpacity style={styles.btnWrapper(type, isRounded)} onPress={onPress}>
+        {(leftIcon !== null) && <Image source={leftIcon} style={styles.leftIcon} />}
+        <Text style={styles.btnText(type)}>{title}</Text>
+        {(rightIcon !== null) && <Image source={rightIcon} style={styles.rightIcon} />}
+      </TouchableOpacity>
+    )
+  } else {
+    return (
+      <TouchableOpacity style={styles.btnWrapperIcon(type, size)} onPress={onPress}>
+        <Image source={icon} style={styles.btnIcon(size)} />
+      </TouchableOpacity>
+    )
+  }
 }
 
 export default Button
 
 const styles = StyleSheet.create({
   btnWrapper: (type, isRounded) => ({
-    flexDirection: 'row',
-    paddingVertical: 10,
-    alignItems: 'center',
     backgroundColor: (type === 'primary') ? COLOR.BUTTON.PRIMARY : COLOR.BUTTON.SECONDARY,
-    borderRadius: isRounded && 25,
-    elevation: 1,
+    borderColor: (type === 'secondary') && COLOR.BUTTON.PRIMARY,
     borderWidth: (type === 'secondary') && 1,
-    borderColor: (type === 'secondary') && COLOR.TEXT.PRIMARY
+    borderRadius: isRounded && 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 10,
+    position: 'relative',
   }),
   btnText: (type) => ({
-    fontFamily: FONT.NORMAL,
-    color: COLOR.TEXT.PRIMARY
+    color: (type === 'primary') ? COLOR.WHITE : COLOR.BUTTON.PRIMARY,
+    fontFamily: FONT[500],
+    fontSize: 14,
+  }),
+  leftIcon: {
+    position: 'absolute',
+    height: 20,
+    width: 20,
+    left: 20,
+  },
+  rightIcon: {
+    position: 'absolute',
+    height: 20,
+    right: 20,
+    width: 20,
+  },
+  btnWrapperIcon: (type, size) => ({
+    backgroundColor: (type === 'primary') ? COLOR.BUTTON.PRIMARY : COLOR.BUTTON.SECONDARY,
+    borderColor: (type === 'secondary') && COLOR.BUTTON.PRIMARY,
+    borderWidth: (type === 'secondary') && 1,
+    backgroundColor: COLOR.BUTTON.PRIMARY,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 6,
+    height: size,
+    width: size,
+  }),
+  btnIcon: (size) => ({
+    height: size - 10,
+    width: size - 10,
   })
 })
--- template/src/screens/docs/DButton.js
+++ template/src/screens/docs/DButton.js
@@ -1,11 +1,74 @@
 import React from 'react'
-import { ScrollView, StyleSheet } from 'react-native'
-import { Navbar } from '../../components'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Button, Gap, Navbar } from '../../components'
+import { IconRight, IconSearch } from '../../assets'
+import { COLOR, FONT } from '../../theme'
 
 const DButton = () => {
   return (
     <ScrollView style={styles.page}>
       <Navbar title="Button" withBack />
+      
+      {/* Standart Button */}
+      <View style={styles.cardWrapper}>
+        <Text style={styles.cardLabel}>Standart Button</Text>
+        <Gap height={15} />
+        <Text style={styles.cardContent}>Primary</Text>
+        <Gap height={5} />
+        <Button title='Click Here!' />
+        <Gap height={10} />
+        <Text style={styles.cardContent}>Secondary</Text>
+        <Gap height={5} />
+        <Button title='Click Here!' type='secondary' />
+        <Gap height={10} />
+      </View>
+      {/* End of Standart Button */}
+
+      {/* Rounded Button */}
+      <View style={styles.cardWrapper}>
+        <Text style={styles.cardLabel}>Rounded Button</Text>
+        <Gap height={15} />
+        <Text style={styles.cardContent}>Primary</Text>
+        <Gap height={5} />
+        <Button title='Click Here!' isRounded />
+        <Gap height={10} />
+        <Text style={styles.cardContent}>Secondary</Text>
+        <Gap height={5} />
+        <Button title='Click Here!' type='secondary' isRounded />
+        <Gap height={10} />
+      </View>
+      {/* End of Rounded Button */}
+
+      {/* Button With Icon */}
+      <View style={styles.cardWrapper}>
+        <Text style={styles.cardLabel}>Button With Icon</Text>
+        <Gap height={15} />
+        <Text style={styles.cardContent}>Primary</Text>
+        <Gap height={5} />
+        <Button title='Click Here!' leftIcon={IconSearch} rightIcon={IconRight} />
+        <Gap height={10} />
+        <Text style={styles.cardContent}>Secondary</Text>
+        <Gap height={5} />
+        <Button title='Click Here!' type='secondary' leftIcon={IconSearch} rightIcon={IconRight} />
+        <Gap height={10} />
+      </View>
+      {/* End of Button With Icon */}
+
+      {/* Icon Button */}
+      <View style={styles.cardWrapper}>
+        <Text style={styles.cardLabel}>Icon Button</Text>
+        <Gap height={15} />
+        <Text style={styles.cardContent}>Primary</Text>
+        <Gap height={5} />
+        <Button title='Click Here!' icon={IconSearch} size={32} />
+        <Gap height={10} />
+        <Text style={styles.cardContent}>Secondary</Text>
+        <Gap height={5} />
+        <Button title='Click Here!' type='secondary' icon={IconSearch} size={32} />
+        <Gap height={10} />
+      </View>
+      {/* End of Icon Button */}
+
     </ScrollView>
   )
 }
@@ -14,6 +77,24 @@ export default DButton
 
 const styles = StyleSheet.create({
   page: {
-    flex: 1
+    flex: 1,
   },
+  cardWrapper: {
+    backgroundColor: COLOR.WHITE,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginBottom: 5,
+    borderRadius: 5,
+    elevation: 1,
+  },
+  cardLabel: {
+    color: COLOR.TEXT.PRIMARY,
+    fontFamily: FONT[700],
+    fontSize: 12,
+  },
+  cardContent: {
+    color: COLOR.TEXT.PRIMARY,
+    fontFamily: FONT.NORMAL,
+    fontSize: 12,
+  }
 })
